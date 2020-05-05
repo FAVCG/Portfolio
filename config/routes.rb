e1657f82f3ea7aab3852a36ea52c5ae91c7883bf
Faviola Caballero <fabycg18@gmail.com>
2020-05-04 23:15:56 -0700

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
    get 'portfolio' => 'portfolio#portfolio'
    get 'about' => 'about#about'
    get 'contact' => 'contact#contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
